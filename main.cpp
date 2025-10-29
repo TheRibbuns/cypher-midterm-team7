dd21e127c79eb7d7294b0963a651aeb9fc3f3ab9
Shakin <xity6645@gmail.com>
2025-10-29 02:08:12 -0400

    more testing + new files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//your code goes here :}
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <ctime> //gonna need thus for randomizing later?
+
+using namespace std;
+
+int main() {
+
+    string encrypted;
+    vector<char> decrypted;
+    int key;
+    int i;
+    // subject to change i was just testing
+
+    return 0;
+}
