8faea0720c628a076bb2a024e40e8f21f4b6806b
Shifureh <Shifatulg@gmail.com>
2025-10-29 12:54:18 -0400

    Frequency Table #2
    Extracted the code into a function for readability + changed array into a vector

--- main.cpp
+++ main.cpp
@@ -16,23 +16,26 @@ char getCharFromIndex(int x) {
     return (char)(x + 65);
 }
 
-int main() {
-    string encrypted = "EHOE YAVC EIME WEXK TNOL LSXS AROX PNCM ZNCA SADI GEBX ZOBM RIXE EEKR JTRM YGSX NAXH ZWRE EEFI CWOO YOGL ZWDS ZRNI CIDX ZPOV QOBQ TTME YFYP WOGE YAVC DICF FTSX SACR ZPYA PRYJ LNDM NIZE EIXK LNIE YAVC EIME WROP LTSS YSYV EREX SSSX DPBS GIXG PICX ZACW TSDY DIXQ LKSR RAFE TLKF WEGL LTGI LROE WROE OYKG BUKM YTOH HIDL EHOE YAVC EIME WEXK TNOA PAFI DAVK PBBE TCZE ETOV YSTY DTKW EHON LCAY LRNP ZOWA PAFI DFVS HEBW LNNP PAFI D";
-    int frequencyTable[26] = { 0 };
-    for (char c : encrypted) { 
+vector<int> populateFreqArray(string s) {
+    vector<int> freq(26, 0);
+    for (char c : s) {
         if (isalpha(c)) {
-            frequencyTable[getIndexFromChar(c)] += 1;
+            freq[getIndexFromChar(c)]++;
         }
     }
-    /* 
+    return freq;
+    /*
         for each loop
         https://www.codecademy.com/resources/docs/cpp/loops/foreach
     */
+}
 
+int main() {
+    string encrypted = "EHOE YAVC EIME WEXK TNOL LSXS AROX PNCM ZNCA SADI GEBX ZOBM RIXE EEKR JTRM YGSX NAXH ZWRE EEFI CWOO YOGL ZWDS ZRNI CIDX ZPOV QOBQ TTME YFYP WOGE YAVC DICF FTSX SACR ZPYA PRYJ LNDM NIZE EIXK LNIE YAVC EIME WROP LTSS YSYV EREX SSSX DPBS GIXG PICX ZACW TSDY DIXQ LKSR RAFE TLKF WEGL LTGI LROE WROE OYKG BUKM YTOH HIDL EHOE YAVC EIME WEXK TNOA PAFI DAVK PBBE TCZE ETOV YSTY DTKW EHON LCAY LRNP ZOWA PAFI DFVS HEBW LNNP PAFI D";
+    vector<int> alphaFrequency = populateFreqArray(encrypted);
     for (int i = 0; i < 26; i++) {
-        cout << getCharFromIndex(i) << ": " << frequencyTable[i] << endl;
+        cout << getCharFromIndex(i) << ": " << alphaFrequency[i] << endl;
     }
-
     return 0;
 }
 
