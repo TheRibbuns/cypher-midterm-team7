5a60af122ea343ddedb254002789a7e401e3e04e
Shifureh <Shifatulg@gmail.com>
2025-10-29 12:24:42 -0400

    Compiler + Frequency Table
    Im going to be coding in bash/terminal because I forgot how to use vscode guys. lmk if u need git help

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cypher-midterm-team7
-we got this
+
+Hello guys shifat here
+press ctrl + shift + b to compile
+or cmd + shift + b
+then in the terminal write ./main
+
+ hello professor!
+ 
+ the json part of the compiler I learned last year in a high school class called IT Infrastructure + big data and I just transferred it over.
--- main.cpp
+++ main.cpp
@@ -8,13 +8,31 @@
 
 using namespace std;
 
-int main() {
+int getIndexFromChar(char ch) {
+    return (int)ch - 65;
+}
 
-    string encrypted;
-    vector<char> decrypted;
-    int key;
-    int i;
-    // subject to change i was just testing
+char getCharFromIndex(int x) {
+    return (char)(x + 65);
+}
+
+int main() {
+    string encrypted = "EHOE YAVC EIME WEXK TNOL LSXS AROX PNCM ZNCA SADI GEBX ZOBM RIXE EEKR JTRM YGSX NAXH ZWRE EEFI CWOO YOGL ZWDS ZRNI CIDX ZPOV QOBQ TTME YFYP WOGE YAVC DICF FTSX SACR ZPYA PRYJ LNDM NIZE EIXK LNIE YAVC EIME WROP LTSS YSYV EREX SSSX DPBS GIXG PICX ZACW TSDY DIXQ LKSR RAFE TLKF WEGL LTGI LROE WROE OYKG BUKM YTOH HIDL EHOE YAVC EIME WEXK TNOA PAFI DAVK PBBE TCZE ETOV YSTY DTKW EHON LCAY LRNP ZOWA PAFI DFVS HEBW LNNP PAFI D";
+    int frequencyTable[26] = { 0 };
+    for (char c : encrypted) { 
+        if (isalpha(c)) {
+            frequencyTable[getIndexFromChar(c)] += 1;
+        }
+    }
+    /* 
+        for each loop
+        https://www.codecademy.com/resources/docs/cpp/loops/foreach
+    */
+
+    for (int i = 0; i < 26; i++) {
+        cout << getCharFromIndex(i) << ": " << frequencyTable[i] << endl;
+    }
 
     return 0;
 }
+
