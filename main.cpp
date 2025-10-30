752e5ac8c3dcecfd1baf6ae427aad0f0b5bc365f
Shifureh <Shifatulg@gmail.com>
2025-10-29 21:11:38 -0400

    Vigenere Function Fixed + Caesar Cipher
    The vigenere function was a bit flawed and fixed by Dwann

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int getIndexFromChar(char ch) {
 }
 
 char getCharFromIndex(int x) {
-    return (char)(x + 65);
+    return (char)((x % 26 + 65));
 }
 
 vector<int> populateFreqArray(string s) {
@@ -38,23 +38,31 @@ string vigenereShiftPartOne(string s) {
         stringToIndex.at(i) = getIndexFromChar(key.at(i)); // turns the words into there number counterpart and into an array
     }
 
-    for (int i = 0, j = 0; i < s.length(); i++, j++) {
+    for (int i = 0, j = 0; i < s.length(); i++) {
         if (isalpha(s.at(i))) {
             int shift = stringToIndex.at(j % key.length()); // gets the index from shifter array with j and mod.
             s.at(i) = getCharFromIndex((getIndexFromChar(s.at(i)) + shift) % 26);  // replaces the character at index i with the key and at index j.
+            j++;
         }
     }
     return s;
 }
 
+void caesarCipher(string s, int x) {
+    for (int i = 0; i < s.length(); i++) {
+        if (isalpha(s.at(i))) {
+            cout << getCharFromIndex(getIndexFromChar(s.at(i)) + x);
+        } else {
+            cout << " ";
+        }
+    }
+}
+
 int main() {
     string encrypted = "EHOE YAVC EIME WEXK TNOL LSXS AROX PNCM ZNCA SADI GEBX ZOBM RIXE EEKR JTRM YGSX NAXH ZWRE EEFI CWOO YOGL ZWDS ZRNI CIDX ZPOV QOBQ TTME YFYP WOGE YAVC DICF FTSX SACR ZPYA PRYJ LNDM NIZE EIXK LNIE YAVC EIME WROP LTSS YSYV EREX SSSX DPBS GIXG PICX ZACW TSDY DIXQ LKSR RAFE TLKF WEGL LTGI LROE WROE OYKG BUKM YTOH HIDL EHOE YAVC EIME WEXK TNOA PAFI DAVK PBBE TCZE ETOV YSTY DTKW EHON LCAY LRNP ZOWA PAFI DFVS HEBW LNNP PAFI D";
-    vector<int> alphaFrequency = populateFreqArray(encrypted);
-    // for (int i = 0; i < 26; i++) {
-    //     cout << getCharFromIndex(i) << ": " << alphaFrequency[i] << endl;
-    // }
     string vigenereShiftedString = vigenereShiftPartOne(encrypted);
-    cout << vigenereShiftedString << endl;
+    vector<int> vigenereFreq = populateFreqArray(vigenereShiftedString);
+    caesarCipher(vigenereShiftedString, 1);
 
     return 0;
 }
